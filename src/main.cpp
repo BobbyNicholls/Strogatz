ce36a5910622b2d9f7f05e16b73bfb2911b0e028
tsameti <robert.r.nicholls@gmail.com>
2022-08-06 17:22:15 +0100

    fixed bug where window clearing and drawing was in the event while loop put movement updating inside a thing that checks a 50th of a second has passed

--- src/main.cpp
+++ src/main.cpp
@@ -16,19 +16,54 @@ int main()
     std::cout << entity1.get_id() << '\n';
     entity.propagate_beliefs();
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Strogatz");
-    window.setActive(true);
+    const int game_width = 800;
+    const int game_height = 600;
+    const int edge_buffer = 10;
+    const float move_speed = 150.f;
+    
+    sf::RenderWindow window(sf::VideoMode(game_width, game_height), "Strogatz");
     window.setVerticalSyncEnabled(true);
     // Never use both setVerticalSyncEnabled and setFramerateLimit at the 
     // Same time! They would badly mix and make things worse.
-    //window.setFramerateLimit(60); //for if we want a specific frame rate,
+    // window.setFramerateLimit(60); //for if we want a specific frame rate,
     // not 100% reliable, especially for high framerates
     sf::Text title_text;
     sf::Font title_font;
     get_title_text("Welcome to Strogatz", title_text, title_font);
+    
+    sf::Clock clock;
+    float time_counter{ 0 };
+    constexpr float time_step = 1.0f / 60.0f;
+
     // run the program as long as the window is open
     while (window.isOpen())
     {
+        time_counter += clock.restart().asSeconds();
+        
+        if (time_counter >= time_step)
+        {
+            sf::Vector2f pos{ title_text.getPosition() };
+            float move_distance{ move_speed * time_counter };
+            time_counter -= time_step;
+            //std::cout << move_distance << '\n';
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            {
+                title_text.move((pos.x < edge_buffer) ? move_distance : -move_distance, 0.f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            {
+                title_text.move((pos.x > (game_width - edge_buffer)) ? -move_distance : move_distance, 0.f);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            {
+                title_text.move(0.f, (pos.y < edge_buffer) ? move_distance : -move_distance);
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            {
+                title_text.move(0.f, (pos.y > (game_height - edge_buffer)) ? -move_distance : move_distance);
+            }
+        }
+
         // Check all the window's events that were triggered since the last
         // iteration of the loop.
         sf::Event event;
@@ -38,14 +73,12 @@ int main()
         {
             switch (event.type)
             {
-                // window closed
             case sf::Event::Closed:
                 // can put "are you sure?" code here
                 window.close();
                 break;
 
-            case sf::Event::LostFocus: // when the window is no longer the
-                // active window
+            case sf::Event::LostFocus:
                 //myGame.pause();
                 break;
 
@@ -55,29 +88,11 @@ int main()
 
             case sf::Event::KeyPressed:
                 {
-                    sf::Vector2f pos{ title_text.getPosition() };
-                    if (event.key.code == sf::Keyboard::D)
-                    {
-                        title_text.move((pos.x > 750.f) ? -5.f : 5.f, 0.f);
-                    }
-                    if (event.key.code == sf::Keyboard::A)
-                    {
-                        title_text.move((pos.x < 50.f) ? 5.f : -5.f, 0.f);
-                    }
-                    if (event.key.code == sf::Keyboard::S)
-                    {
-                        title_text.move(0.f, (pos.y > 550.f) ? -5.f : 5.f);
-                    }
-                    if (event.key.code == sf::Keyboard::W)
-                    {
-                        title_text.move(0.f, (pos.y < 50.f) ? 5.f : -5.f);
-                    }
-
-                    if (event.key.code == sf::Keyboard::Escape)
-                    {
-                        std::cout << "the escape key was pressed" << '\n';
-                    }
-                    break;
+                if (event.key.code == sf::Keyboard::Escape)
+                {
+                    std::cout << "the escape key was pressed" << '\n';
+                }
+                break;
                 }
             case sf::Event::MouseButtonPressed:
             {
@@ -97,41 +112,43 @@ int main()
             default:
                 break;
             }
-            // Clear the window with black color (doesnt activate until 
-            // window.display() so has no immediate impact)
-            window.clear(sf::Color::Black);
-            /*
-            
-            This clear/draw/display cycle is the only good way to draw 
-            things. Don't try other strategies, such as keeping pixels 
-            from the previous 
-            frame, "erasing" pixels, or drawing once and calling display 
-            multiple times. You'll get strange results due to 
-            double-buffering.
 
-            Modern graphics hardware and APIs are really made for 
-            repeated clear/draw/display cycles where everything is 
-            completely refreshed at each iteration of the main loop. 
-            Don't be scared to draw 1000 sprites 60 times per second, 
-            you're far below the millions of triangles that your 
-            computer can handle.
-            
-            */
-            // end the current frame and takes what was drawn since the 
-            // last call to display and displays it on the window
-            
-            // to draw to a texture instead of a window: 
-            // https://www.sfml-dev.org/tutorials/2.5/graphics-draw.php#off-screen-drawing
-            // for multi-threaded drawing: 
-            // https://www.sfml-dev.org/tutorials/2.5/graphics-draw.php#drawing-from-threads
-            // this is so you can do event handling in the main loop's
-            // thread (which is advised) and rendering in another thread.
-            // This is a good idea.
-            
-            window.draw(title_text);
-            
-            window.display();
         }
+
+        // Clear the window with black color (doesnt activate until 
+        // window.display() so has no immediate impact)
+        window.clear(sf::Color::Black);
+        /*
+
+        This clear/draw/display cycle is the only good way to draw
+        things. Don't try other strategies, such as keeping pixels
+        from the previous
+        frame, "erasing" pixels, or drawing once and calling display
+        multiple times. You'll get strange results due to
+        double-buffering.
+
+        Modern graphics hardware and APIs are really made for
+        repeated clear/draw/display cycles where everything is
+        completely refreshed at each iteration of the main loop.
+        Don't be scared to draw 1000 sprites 60 times per second,
+        you're far below the millions of triangles that your
+        computer can handle.
+
+        */
+        // end the current frame and takes what was drawn since the 
+        // last call to display and displays it on the window
+
+        // to draw to a texture instead of a window: 
+        // https://www.sfml-dev.org/tutorials/2.5/graphics-draw.php#off-screen-drawing
+        // for multi-threaded drawing: 
+        // https://www.sfml-dev.org/tutorials/2.5/graphics-draw.php#drawing-from-threads
+        // this is so you can do event handling in the main loop's
+        // thread (which is advised) and rendering in another thread.
+        // This is a good idea.
+
+        window.draw(title_text);
+
+        window.display();
     }
     
     return 0;
