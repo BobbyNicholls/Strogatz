363da0aadb1e530c99887f17266f19a7d798f072
tsameti <robert.r.nicholls@gmail.com>
2022-08-09 21:14:51 +0100

    added implementation of EntityCircle which was accidentally not icnluded in the git repo before

---
+++ src/EntityCircle.cpp
@@ -0,0 +1,19 @@
+#include "EntityCircle.h"
+
+EntityCircle::EntityCircle(
+	float radius,
+	float outline_thickness, 
+	sf::Color fill_colour,
+	sf::Color outline_colour
+)
+	: m_radius{ radius },
+	m_outline_thickness{ outline_thickness }, 
+	m_fill_colour{ fill_colour }, 
+	m_outline_colour{ outline_colour }
+{
+	m_shape.setRadius(m_radius);
+	m_shape.setFillColor(m_fill_colour);
+	m_shape.setOutlineThickness(m_outline_thickness);
+	m_shape.setOutlineColor(m_outline_colour);
+	m_shape.setPosition(10.f, 50.f);
+}
