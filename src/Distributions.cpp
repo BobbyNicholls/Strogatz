9111d8f9a9b3e9c236dd252cfa49a298d02ee5ad
tsameti <robert.r.nicholls@gmail.com>
2022-08-18 20:52:40 +0100

    added distributions file

---
+++ src/Distributions.cpp
@@ -0,0 +1,47 @@
+#include <cstdlib>
+#include <random>
+
+#include "Distributions.h"
+
+std::mt19937 mt{ std::random_device{}() };
+
+
+float mild_aversion(float degrees_of_freedom)
+{
+	static std::chi_squared_distribution<float> chi2{ degrees_of_freedom };
+	return chi2(mt);
+}
+
+
+float mild_affinity()
+{
+	return std::max(0.00001f, 10 - mild_aversion());;
+}
+
+
+float strong_aversion(float degrees_of_freedom)
+{
+	static std::chi_squared_distribution<float> chi2{ degrees_of_freedom };
+	return chi2(mt);
+}
+
+
+float strong_affinity()
+{
+	return std::max(0.00001f, 10 - strong_aversion());
+}
+
+
+float uniform_distribution_float(int min, int max)
+{
+	// static used for efficiency, so we only calculate this value once
+	static constexpr double fraction{ 1.0 / (RAND_MAX + 1.0) };
+	return min + static_cast<float>((max - min) * (std::rand() * fraction));
+}
+
+
+int uniform_distribution_int(int min, int max)
+{
+	static constexpr double fraction{ 1.0 / (RAND_MAX + 1.0) };
+	return min + static_cast<int>((max - min + 1) * (std::rand() * fraction));
+}
