698069ffbd1e20877d20c10e16be344237e8e600
tsameti <robert.r.nicholls@gmail.com>
2022-08-23 17:55:07 +0100

    removed old,now redundant, graph getter

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -54,63 +54,6 @@ void add_random_edge(Graph& graph, int max_entitiy_iloc)
 }
 
 
-Graph get_graph(time_period_t time_period)
-{
-    Graph graph{};
-    std::set<unsigned int> link_anchors{ 0 };
-    unsigned int entity_iloc;
-    graph.entities.resize(entities_start_size); // initialise on creation??
-    graph.entities.reserve(entities_reserve_limit);
-    for (int i{ 0 }; i < entities_start_size; ++i)
-    {
-        EntityCircle* entity_pointer{ new (std::nothrow) EntityCircle(i, time_period) };
-        if (!entity_pointer) // handle case where new returned null
-        {
-            // TODO: error handling here
-            std::cerr << "\n\nCould not allocate memory!!!\n\n";
-        }
-        else
-        {
-            graph.entities[i] = entity_pointer;
-            if (i == 0)
-            {
-                graph.entities[i]->set_position_randomly();
-            }
-            else if (i == 1)
-            {
-                link_entities(
-                    graph.entities[0],
-                    graph.entities[1],
-                    graph.links,
-                    graph.link_counter
-                );
-                graph.entities[i]->set_position_relative_to_links();
-            }
-            else // todo: make a set of non-anchors then make it less likely to attch to those
-            {
-                entity_iloc = add_preferential_links(
-                    graph.entities,
-                    graph.entities[i],
-                    graph.links,
-                    graph.link_counter
-                );
-                if (link_anchors.contains(entity_iloc))
-                {
-                    graph.entities[i]->set_position_relative_to_links();
-                }
-                else
-                {
-                    graph.entities[i]->set_position_randomly();
-                    link_anchors.insert(i);
-                }
-            }
-        }
-    }
-
-    return graph;
-}
-
-
 EntityCircle* get_entity_circle(id_t id, time_period_t time_period)
 {
     EntityCircle* entity_pointer{ new (std::nothrow) EntityCircle(id, time_period) };
@@ -268,18 +211,9 @@ void propagate_entities(Graph& graph, time_period_t time_period)
             if (to_entity->is_paired() && (to_entity->get_partner() == from_entity))
             {
                 std::cout << "Paired entity spawning child.\n";
-                EntityCircle* child_entity{ new (std::nothrow) EntityCircle(
-                    static_cast<id_t>(graph.entities.size()), time_period
-                )};
-                if (!child_entity)
-                {
-                    // TODO: error handling here
-                    std::cerr << "\n\nCould not allocate memory!!!\n\n";
-                }
-                else
-                {
-                    graph.entities.push_back(child_entity);
-                }
+                // BUGGED!
+                EntityCircle* child_entity{ get_entity_circle(i, time_period) };
+                graph.entities.push_back(child_entity);
                 from_entity->add_child(child_entity);
                 to_entity->add_child(child_entity);
                 child_entity->add_parents(from_entity, to_entity);
