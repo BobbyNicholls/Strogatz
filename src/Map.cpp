28c074c9c6ee6ab3128d9e6c872e9c87a3cd4024
tsameti <robert.r.nicholls@gmail.com>
2022-10-24 09:14:09 +0100

    updated todo with buf fix for map edge issue

--- src/Map.cpp
+++ src/Map.cpp
@@ -116,7 +116,7 @@ void Map::print_road_grid()
 
 void Map::map_textures_to_road_grid(const int road_texture_columm, const int ground_texture_columm)
 {
-	// the matrix might need padding??
+	// TODO: the matrix needs
 	int convolution_window{};
 	for (int row{ 0 }; row < (m_road_grid.height - 1); ++row)
 	{
